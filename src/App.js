3a0d3581e9dafe65820a1f214b3b9871d2cbf3ab
FLnhardt <fannilinhardt@gmail.com>
2024-01-09 14:01:31 +0100

    tuesday ws

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import { useEffect, useState } from 'react';
 import './App.css';
+import Countries from './components/Countries';
 
 function App() {
+  const [countries, setCountries] = useState(null)
+
+  useEffect(() => {
+    fetch("https://restcountries.com/v3.1/all")
+      .then(res => res.json())
+      .then(data => setTimeout(() => setCountries(data), 1000))
+  }, [])
+
   return (
     <div className="App">
-    
+      {countries ? <Countries countries={countries} /> : <p>loading...</p>}
     </div>
   );
 }
